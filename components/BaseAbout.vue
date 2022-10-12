727883735659042cf6bc0e05051505918d1d2696
Legat48 <daletters01@gmail.com>
2022-10-12 08:48:26 +0300

    add about not full

--- components/BaseAbout.vue
+++ components/BaseAbout.vue
@@ -1,40 +1,42 @@
 <!-- Please remove this file from your project -->
 <template>
   <section id="about" class="about">
-    <ul class="about__list">
-      <li class="about__item about__item_1">
-        <h3 class="about__title">
-          Мы принимаем к оплате банковские карты
-        </h3>
-        <p class="about__text">
-          Для нас важен комфорт клиента, поэтому мы приобрели терминал
-        </p>
-      </li>
-      <li class="about__item about__item_2">
-        <h3 class="about__title">
-          Опытные специалисты
-        </h3>
-        <p class="about__text">
-          У нас работает 5 стоматологов. Каждый является кандидатом медицинских наук
-        </p>
-      </li>
-      <li class="about__item about__item_3">
-        <h3 class="about__title">
-          Современное оборудование
-        </h3>
-        <p class="about__text">
-          По уровню технического оснащения мы входим в 5% лучших клиник Тулы
-        </p>
-      </li>
-      <li class="about__item about__item_4">
-        <h3 class="about__title">
-          Комфорт для пациентов
-        </h3>
-        <p class="about__text">
-          Мы создали такую атмосферу, что визит к стоматологу стал сравним с походом к друзьям в гости
-        </p>
-      </li>
-    </ul>
+    <div class="about__box container">
+      <ul class="about__list">
+        <li class="about__item about__item_1">
+          <h3 class="about__title">
+            Мы принимаем к оплате банковские карты
+          </h3>
+          <p class="about__text">
+            Для нас важен комфорт клиента, поэтому мы приобрели терминал
+          </p>
+        </li>
+        <li class="about__item about__item_2">
+          <h3 class="about__title">
+            Опытные специалисты
+          </h3>
+          <p class="about__text">
+            У нас работает 5 стоматологов. Каждый является кандидатом медицинских наук
+          </p>
+        </li>
+        <li class="about__item about__item_3 about__item_content-right">
+          <h3 class="about__title">
+            Современное оборудование
+          </h3>
+          <p class="about__text">
+            По уровню технического оснащения мы входим в 5% лучших клиник Тулы
+          </p>
+        </li>
+        <li class="about__item about__item_4">
+          <h3 class="about__title">
+            Комфорт для пациентов
+          </h3>
+          <p class="about__text">
+            Мы создали такую атмосферу, что визит к стоматологу стал сравним с походом к друзьям в гости
+          </p>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
