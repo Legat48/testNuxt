ac20560312d767aefc86a53f8404dfeb775dfba4
Legat48 <daletters01@gmail.com>
2022-10-13 15:31:30 +0300

    fix confing

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['checkAuth'] = require('..\\middleware\\checkAuth.js')
-middleware['checkAuth'] = middleware['checkAuth'].default || middleware['checkAuth']
-
-middleware['services'] = require('..\\middleware\\services.js')
-middleware['services'] = middleware['services'].default || middleware['services']
-
-export default middleware
